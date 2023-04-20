597c736ae6f38c088dbee7ffa38f4e2e44f5708b
Monho12 <92088341+Monho12@users.noreply.github.com>
2023-04-20 19:45:50 +0800

    70%

--- chat/src/Client/client.jsx
+++ chat/src/Client/client.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getAuthorizationHeader = () =>
   `${window.localStorage.token ? window.localStorage.token : ""}`;
 export const client = axios.create({
-  baseURL: "http://localhost:7000/",
+  baseURL: "https://chatsysapi.onrender.com/",
   headers: {
     authorization: getAuthorizationHeader(),
   },
